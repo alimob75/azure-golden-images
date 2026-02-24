549f58868aa7e163c8592157cdda4a679d2754ad
alimob75 <129009123+alimob75@users.noreply.github.com>
2026-02-24 23:27:12 +0100

    Improve error handling in prepareLinuxScript.sh
    Add error handling to stop commands and cloud-init cleanup.

--- prepareLinuxScript.sh
+++ prepareLinuxScript.sh
@@ -8,12 +8,18 @@ sudo waagent -deprovision+user -verbose -force -start
 
 sleep 60
 # Stop services
-sudo systemctl stop ssh sshd cloud-init cloud-init-local cloud-init-config cloud-init-final
+sudo systemctl stop ssh || true
+sudo systemctl stop sshd || true
+sudo systemctl stop cloud-init || true
+sudo systemctl stop cloud-init-local || true
+sudo systemctl stop cloud-init-config || true
+sudo systemctl stop cloud-init-final || true
+
 
 # CLOUD-INIT CLEANUP (CRITICAL)
-sudo cloud-init clean --logs
-sudo cloud-init clean
-sudo rm -rf /var/lib/cloud/instances/* /var/lib/cloud/sem/
+sudo cloud-init clean --logs || true
+sudo cloud-init clean || true
+sudo rm -rf /var/lib/cloud/instances/* /var/lib/cloud/sem/ || true
 
 # SSH keys (prevent regeneration)
 sudo rm -f /etc/ssh/ssh_host_*
