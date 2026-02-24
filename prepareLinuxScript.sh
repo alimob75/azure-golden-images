9053a5bc6dadeeeed337add3230ade1b19a54925
alimob75 <129009123+alimob75@users.noreply.github.com>
2026-02-24 15:48:26 +0100

    Enhance Linux VM preparation script
    Added cleanup and shutdown commands for Linux VM preparation.

--- prepareLinuxScript.sh
+++ prepareLinuxScript.sh
@@ -5,3 +5,15 @@ echo "Preparing Linux VM for generalization..."
 
 sudo shutdown +3
 sudo waagent -deprovision+user -verbose -force -start
+
+sleep 60
+# Stop services
+sudo systemctl stop ssh sshd cloud-init cloud-init-local cloud-init-config cloud-init-final
+
+# CLOUD-INIT CLEANUP (CRITICAL)
+sudo cloud-init clean --logs
+sudo cloud-init clean
+sudo rm -rf /var/lib/cloud/instances/* /var/lib/cloud/sem/
+
+# SSH keys (prevent regeneration)
+sudo rm -f /etc/ssh/ssh_host_*
