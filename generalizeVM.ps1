ce0b13953b5ca371cff6e4caa83a527b15e49a07
alimob75 <129009123+alimob75@users.noreply.github.com>
2026-02-24 23:47:58 +0100

    Enhance VM generalization script with cleanup and config
    Added commands to remove Qualys registry value and configure DNS suffixes.

--- generalizeVM.ps1
+++ generalizeVM.ps1
@@ -7,6 +7,12 @@ reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\cdrom /v start /t R
 # Delete panther directory
 Remove-Item -Path C:\Windows\Panther -Recurse -Force -ErrorAction SilentlyContinue
 
+# Delete Qualys registry value
+Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Qualys' -Name 'HostID' -Force -ErrorAction SilentlyContinue
+
+# Configure Dns Suffixes
+Set-DnsClientGlobalSetting -SuffixSearchList @("rs.sdxcorp.net", "ce.sdxcorp.net", "na.sdxcorp.net") -ErrorAction SilentlyContinue
+
 # Run sysprep with /shutdown (not /oobe as per docs)
 Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize'
 
